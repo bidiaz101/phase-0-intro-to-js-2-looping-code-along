5a61a5e0d9a07db6376acd32522d53298992d6c7
Brian Diaz <bidiaz101@gmail.com>
2021-08-12 20:42:14 -0500

    commit tree 174f523ee1f7975105449f645b7a989e355016f2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-let cards = [];
+const cards = [];
 
 function writeCards(names, event) {
-    for (let i = 0; i < names.length; i ++) {
+    for(let i = 0; i < names.length; i++) {
         cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
     }
-
     return cards;
-}
+};
+
 function countDown(integer) {
-    while (integer>=0) {
-        console.log(integer--)
-    }
-}
+    while (integer >= 0) {
+        console.log(integer--);
+    };
+};
